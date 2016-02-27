9045e95ecf08e83e109868fd2cc46acd21e6bf7e
Andrei Vedovato <me@andreivedovato.com>
2016-02-27 01:33:14 -0300

    Normalized files and reset images folder. General commit D:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,5 @@
 (function($){
   $(document).ready(function(){
-    var $slides = $('#slides');
-
-    $slides.superslides({
-      pagination: true,
-      play: 4000,
-      animation: 'fade'
-    })
-
-    Hammer($slides[0]).on("swipeleft", function(e) {
-      $slides.data('superslides').animate('next')
-    })
-
-    Hammer($slides[0]).on("swiperight", function(e) {
-      $slides.data('superslides').animate('prev')
-    })
-
-    $('.slides-container div:first').click(function(){
-      $(this).css('cursor', 'pointer')
-      window.location = 'http://www.estanciadoliveira.com/destaques/alta-gastronomia-com-o-melhor-preco-da-regiao/'
-    })
-
-    // --------------------
-
-    // Search widget. Add text on placeholder
-    $('.widget_search #s').attr("placeholder", "Pesquise aqui...")
-
-  })
-})(jQuery)
+    // jQuery stuff
+  });
+})(jQuery);
